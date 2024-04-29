04dc972e616dd74469ec6f96f226e6880004e37d
MAFTOUH <omar.maftou0000@gmail.com>
2024-04-29 11:29:21 +0100

    préparation envirement

---
+++ app/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+ 
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+}
