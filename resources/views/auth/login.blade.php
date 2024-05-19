f310f0173f8da398266c4405c3ae75ded0359fac
MAFTOUH <omar.maftou0000@gmail.com>
2024-05-19 12:19:05 +0100

    auth are done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,57 +1,94 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-slate-200">
+    <div class="flex flex-col md:flex-row">
+        <div class="flex justify-center items-center w-full md:w-1/2 md:h-screen mt-10">
+            <img src="./assets/icons/SocialSync.svg" class="md:w-[65%] w-[100px]" alt="">
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="flex justify-center items-start w-full md:w-1/2 flex-col md:px-0 px-2">
+            <h1 class="md:text-[64px] md:mx-0 mx-auto text-[30px] mb-2 font-extrabold text-blue-600">Connectez‑vous à <span class="text-black ">Social Sync</span></h1>
+            <!-- Button OAuth -->
+            <a href="{{ route('google-auth') }}" class="w-full md:w-1/2 bg-white shadow-lg mb-1 hover:bg-slate-100 border-2 border-blue-500 rounded-3xl flex items-center justify-center py-2 px-10">
+                <img src="./assets/icons/google.svg" class="w-[20px] h-[20px] mx-2" alt="">
+                <span class="text-slate-400 font-bold">Se connecter avec google</span>
+            </a>
+            <a href="{{ route('github-auth') }}" class="w-full md:w-1/2 bg-white shadow-lg mb-1 hover:bg-slate-100 border-2 border-blue-500 rounded-3xl flex items-center justify-center py-2 px-10">
+                <img src="./assets/icons/github.svg" class="w-[20px] h-[20px] mx-2" alt="">
+                <span class="text-slate-400 font-bold">Se connecter avec github</span>
+            </a>
+            <!-- Login simple -->
+            <span class="text-center text-sm w-full md:w-1/2 my-1">Ou</span>
+            <button id="open-modal-button" class="w-full md:w-1/2 bg-blue-500 hover:bg-blue-400 shadow-lg mb-1 rounded-3xl flex items-center justify-center py-2 px-14" data-modal-toggle="login-modal">
+                <span class="text-white font-bold">Se connecter</span>   
+            </button>
+            <span class="text-center w-full md:w-1/2 font-semibold my-1">Vous n'avez pas de compte ?</span>
+            <a href="{{ route('register') }}" class="w-full md:w-1/2 border-blue-500 bg-white hover:bg-slate-100 border-2 shadow-lg mb-1 rounded-3xl flex items-center justify-center py-2 px-14">
+                <span class="text-blue-500 font-bold">Inscrivez‑vous</span>   
+            </a>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3 bg-green-500 hover:bg-green-400 dark:bg-green-500 hover:dark:bg-green-400 dark:text-white">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-        <div class="text-center my-4">
-            <hr class="my-2">
-            <span class="font-bold text-center dark:text-white">Or</span>
-            <div class="flex items-center justify-center my-2">
-                <a href="{{ route('google-auth') }}"  class="flex items-center bg-white dark:bg-gray-900 border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 dark:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                    <svg class="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="800px" height="800px" viewBox="-0.5 0 48 48" version="1.1"> <title>Google-color</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Color-" transform="translate(-401.000000, -860.000000)"> <g id="Google" transform="translate(401.000000, 860.000000)"> <path d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24" id="Fill-1" fill="#FBBC05"> </path> <path d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333" id="Fill-2" fill="#EB4335"> </path> <path d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667" id="Fill-3" fill="#34A853"> </path> <path d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24" id="Fill-4" fill="#4285F4"> </path> </g> </g> </g> </svg>
-                    <span>Continue with Google</span>
-                </a>
+        <!-- Modal -->
+        <div id="login-modal" class="modal fixed top-0 left-0 w-full h-full flex items-center justify-center hidden">
+            <div class="relative p-4 w-full max-w-2xl max-h-full">
+                <!-- Modal content -->
+                <div class="relative bg-white border-2 shadow-lg rounded-3xl">
+                    <!-- Modal header -->
+                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+                        <h3 class="text-xl font-semibold text-black">
+                            Connectez‑vous à SocialSync
+                        </h3>
+                        <button id="close-modal-button" type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <!-- Modal content -->
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="flex justify-center items-center mt-4 mx-5 flex-col">
+                            <!-- Email Address -->
+                            <div class="w-full mx-1">
+                                <label>Email</label>
+                                <input type="email" name="email" placeholder="Adresse email" required class="rounded-xl border-2 border-blue-500 w-full">
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
+                            <!-- password -->
+                            <div class="w-full mx-1">
+                                <label>Mot de passe</label>
+                                <input type="password" name="password" placeholder="Mot de passe" required class="rounded-xl py-2 px-2 border-2 border-blue-500 w-full">
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            </div>
+                        </div>
+                    <!-- Modal footer -->
+                    <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b mt-3">
+                        <button type="submit" class="text-white bg-blue-500 hover:bg-blue-400 rounded-3xl w-full py-2.5 font-extrabold ">Suivant</button>
+                    </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </form>
-</x-guest-layout>
+    </div>
+</body>
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const openModalButton = document.getElementById('open-modal-button');
+        const closeModalButton = document.getElementById('close-modal-button');
+        const modal = document.getElementById('login-modal');
+
+        openModalButton.addEventListener('click', () => {
+            modal.classList.remove('hidden');
+        });
+
+        closeModalButton.addEventListener('click', () => {
+            modal.classList.add('hidden');
+        });
+    });
+</script>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,40 +5,31 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite('resources/css/app.css')
-    <style>
-        .poppins {
-            font-family: 'Poppins', sans-serif;
-        }
-
-        .syne {
-            font-family: 'Syne', sans-serif;
-        }
-    </style>
 </head>
 <body class="bg-slate-200">
-    <div class="flex">
-        <div class="flex justify-center items-center md:w-1/2 h-screen">
-            <img src="./assets/icons/SocialSync.svg" class="md:w-[65%]" alt="">
+    <div class="flex flex-col md:flex-row">
+        <div class="flex justify-center items-center w-full md:w-1/2 md:h-screen mt-10">
+            <img src="./assets/icons/SocialSync.svg" class="md:w-[65%] w-[100px]" alt="">
         </div>
-        <div class="flex justify-center items-start md:w-1/2 flex-col">
-            <h1 class="text-[64px] mb-2 font-extrabold text-blue-600">Ça se passe maintenant</h1>
-            <p class="text-[31px] mb-4 font-extrabold text-blue-400">Inscrivez‑vous.</p>
+        <div class="flex justify-center items-start w-full md:w-1/2 flex-col md:px-0 px-2">
+            <h1 class="md:text-[64px] md:mx-0 mx-auto text-[30px] mb-2 font-extrabold text-blue-600">Ça se passe maintenant</h1>
+            <p class="md:text-[31px] md:mx-0 mx-auto text-[16px] mb-4 font-extrabold text-blue-400">Inscrivez‑vous.</p>
             <!-- Button OAuth -->
-            <a href="{{ route('google-auth') }}" class="w-1/2 bg-white shadow-lg mb-1 hover:bg-slate-100 border-2 border-blue-500 rounded-3xl flex items-center justify-center py-2 px-14">
+            <a href="{{ route('google-auth') }}" class="w-full md:w-1/2 bg-white shadow-lg mb-1 hover:bg-slate-100 border-2 border-blue-500 rounded-3xl flex items-center justify-center py-2 px-14">
                 <img src="./assets/icons/google.svg" class="w-[20px] h-[20px] mx-2" alt="">
                 <span class="text-slate-400 font-bold">S'inscrire avec google</span>
             </a>
-            <a href="{{ route('github-auth') }}" class="w-1/2 bg-white shadow-lg mb-1 hover:bg-slate-100 border-2 border-blue-500 rounded-3xl flex items-center justify-center py-2 px-14">
+            <a href="{{ route('github-auth') }}" class="w-full md:w-1/2 bg-white shadow-lg mb-1 hover:bg-slate-100 border-2 border-blue-500 rounded-3xl flex items-center justify-center py-2 px-14">
                 <img src="./assets/icons/github.svg" class="w-[20px] h-[20px] mx-2" alt="">
                 <span class="text-slate-400 font-bold">S'inscrire avec github</span>
             </a>
             <!-- Register simple -->
-            <span class="text-center text-sm w-1/2 my-1">Ou</span>
-            <button id="open-modal-button" class="w-1/2 bg-blue-500 hover:bg-blue-400 shadow-lg mb-1 rounded-3xl flex items-center justify-center py-2 px-14" data-modal-toggle="register-modal">
+            <span class="text-center text-sm w-full md:w-1/2 my-1">Ou</span>
+            <button id="open-modal-button" class="w-full md:w-1/2 bg-blue-500 hover:bg-blue-400 shadow-lg mb-1 rounded-3xl flex items-center justify-center py-2 px-14" data-modal-toggle="register-modal">
                 <span class="text-white font-bold">Créer un compte</span>   
             </button>
-            <span class="text-center w-1/2 font-semibold my-1">Vous avez déjà un compte ?</span>
-            <a href="{{ route('login') }}" class="w-1/2 border-blue-500 bg-white hover:bg-slate-100 border-2 shadow-lg mb-1 rounded-3xl flex items-center justify-center py-2 px-14">
+            <span class="text-center w-full md:w-1/2 font-semibold my-1">Vous avez déjà un compte ?</span>
+            <a href="{{ route('login') }}" class="w-full md:w-1/2 border-blue-500 bg-white hover:bg-slate-100 border-2 shadow-lg mb-1 rounded-3xl flex items-center justify-center py-2 px-14">
                 <span class="text-blue-500 font-bold">Se connecter</span>   
             </a>
         </div>
