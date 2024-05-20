390e9ef61fb2e862dc7422d8391a6d27230179d9
GeekHamza1 <thespecialistpc@outlook.com>
2024-05-20 05:02:40 +0100

    fixed redirection to home after github login

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -43,7 +43,7 @@ class GithubController extends Controller
             Auth::login($user);
 
             // Rediriger vers le tableau de bord
-            return redirect()->intended('homr');
+            return redirect()->intended('home');
         } catch ( \Throwable $th ){
             dd('Something went wrong! ' . $th->getMessage());
         }
