7572d375cedaa1ae25e6e13aa6bfd1678f83e5ce
MAFTOUH <omar.maftou0000@gmail.com>
2024-06-20 16:20:42 +0100

    Migration are done 01

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fullName');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('password')->nullable();
-            $table->string('profile')->nullable();
-            $table->string('phone_number')->nullable()->unique();
-            $table->string('country')->nullable();
-            $table->string('google_id')->nullable();
-            $table->string('github_id')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
