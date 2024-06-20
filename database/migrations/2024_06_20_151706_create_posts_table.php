44974f8ff806237ce726464e506f1c1aa9f877db
MAFTOUH <omar.maftou0000@gmail.com>
2024-06-20 16:19:24 +0100

    Migration are done

--- database/migrations/2024_06_20_151706_create_posts_table.php
+++ database/migrations/2024_06_20_151706_create_posts_table.php
@@ -9,27 +9,24 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('post_name');
-            $table->enum('type', ['text', 'image', 'video']);
-            $table->text('content')->nullable();
-            $table->string('image')->nullable();
-            $table->unsignedInteger('likes_number')->default(0);
-            $table->unsignedInteger('shares_number')->default(0);
+            $table->unsignedBigInteger('user_id');
+            $table->text('post')->nullable();
+            $table->text('content');
             $table->timestamps();
+        
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
-
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('posts');
     }
 };
